d99f9b1bfacb32c1256795ae0f7d11c96a475f50
agusnieto77 <agustin.nieto77@gmail.com>
2024-05-11 01:06:28 -0300

    up

--- R/getTweetsTimeline.R
+++ R/getTweetsTimeline.R
@@ -1,27 +1,33 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   https://r-pkgs.org
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' Get Tweets from User Timeline
+#'
+#' Esta función recupera tweets del timeline de un usuario especificado en Twitter.
+#'
+#' @param username El nombre de usuario de Twitter del cual quieres obtener el timeline.
+#' @param n_tweets El número máximo de tweets a obtener. Por defecto es 100.
+#' @param xuser Nombre de usuario de Twitter para autenticación. Por defecto es el valor de la variable de entorno del sistema USER.
+#' @param xpass Contraseña de Twitter para autenticación. Por defecto es el valor de la variable de entorno del sistema PASS.
+#' @return Un vector que contiene los tweets obtenidos.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' getTweetsTimeline(username = "rstatstweet", n_tweets = 200)
+#' }
+#'
+#' @references
+#' Puedes encontrar más información sobre el paquete TweetScrapeR en:
+#' \url{https://github.com/agusnieto77/TweetScraperR}
+#'
+#' @import rvest
 
 getTweetsTimeline <- function(
     username,
+    n_tweets = 100,
     xuser = Sys.getenv("USER"),
     xpass = Sys.getenv("PASS")
                               ) {
-  twitter <- read_html_live("https://twitter.com/i/flow/login")
-  
+  twitter <- rvest::read_html_live("https://twitter.com/i/flow/login")
   Sys.sleep(3)
-  
   input_1 <- "#layers > div > div > div > div > div > div > div.css-175oi2r.r-1ny4l3l.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv.r-1awozwy > div.css-175oi2r.r-1wbh5a2.r-htvplk.r-1udh08x.r-1867qdf.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1 > div > div > div.css-175oi2r.r-1ny4l3l.r-6koalj.r-16y2uox.r-14lw9ot.r-1wbh5a2 > div.css-175oi2r.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-13qz1uu.r-1ye8kvj > div > div > div > div.css-175oi2r.r-1f1sjgu.r-mk0yit.r-13qz1uu > label > div > div.css-175oi2r.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div > input"
   clic__1 <- "#layers > div > div > div > div > div > div > div.css-175oi2r.r-1ny4l3l.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv.r-1awozwy > div.css-175oi2r.r-1wbh5a2.r-htvplk.r-1udh08x.r-1867qdf.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1 > div > div > div.css-175oi2r.r-1ny4l3l.r-6koalj.r-16y2uox.r-14lw9ot.r-1wbh5a2 > div.css-175oi2r.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-13qz1uu.r-1ye8kvj > div > div > div > div:nth-child(6) > div"
   input_2 <- "#layers > div > div > div > div > div > div > div.css-175oi2r.r-1ny4l3l.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv.r-1awozwy > div.css-175oi2r.r-1wbh5a2.r-htvplk.r-1udh08x.r-1867qdf.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1 > div > div > div.css-175oi2r.r-1ny4l3l.r-6koalj.r-16y2uox.r-14lw9ot.r-1wbh5a2 > div.css-175oi2r.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-13qz1uu.r-1ye8kvj > div.css-175oi2r.r-16y2uox.r-1wbh5a2.r-1dqxon3 > div > div > div.css-175oi2r.r-mk0yit.r-13qz1uu > div > label > div > div.css-175oi2r.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div.css-1rynq56.r-bcqeeo.r-qvutc0.r-37j5jr.r-135wba7.r-16dba41.r-1awozwy.r-6koalj.r-1inkyih.r-13qz1uu > input"
@@ -30,4 +36,24 @@ getTweetsTimeline <- function(
   twitter$click(css = clic__1, n_clicks = 1)
   twitter$type(css = input_2, text = Sys.getenv("PASS"))
   twitter$click(css = iniciar, n_clicks = 1)
+  tweet <- ".css-1rynq56.r-8akbws.r-krxsd3.r-dnmrzs.r-1udh08x.r-bcqeeo.r-qvutc0.r-37j5jr.r-a023e6.r-rjixqe.r-16dba41.r-bnwqim"
+  timeline <- rvest::read_html_live(paste0("https://twitter.com/", username))
+  Sys.sleep(3)
+  tweets_r <- c()
+  i <- 1
+  while (TRUE) {
+    tweets <- rvest::html_text(timeline$html_elements(css = tweet))
+    if (length(tweets_r) > n_tweets) {
+      cat("Finalizó la recolección de tweets.")
+      break
+    }
+    tweets_r <- unique(append(tweets_r, tweets))
+    timeline$scroll_by(top = 4000, left = 0)
+    message("Tweets recolectados: ", length(tweets_r))
+    Sys.sleep(2.5)
+    i <- i + 1
+  }
+  twitter$session$close()
+  timeline$session$close()
+  saveRDS(tweets_r, "timeline.rds")
 }

8bd94dca337912321f357564915700cfb5be65e1
agusnieto77 <agustin.nieto77@gmail.com>
2024-06-01 13:27:13 -0300

    up

--- R/getTweetsSearchStreaming.R
+++ R/getTweetsSearchStreaming.R
@@ -145,6 +145,7 @@ getTweetsSearchStreaming <- function(
     tweets_recolectados <- tweets_recolectados[!is.na(tweets_recolectados$fecha), ]
     saveRDS(tweets_recolectados, paste0(dir, "/tweets_search_", gsub("\\s|#", "", search), "_", gsub("-|:|\\.", "_", format(Sys.time(), "%Y_%m_%d_%X")), ".rds"))
     cat("Datos procesados y guardados.\n")
+    cat("Tweets únicos reolectados:", length(tweets_recolectados$url), "\n")
     return(tweets_recolectados)
   } else {
     cat("No hay tweets para procesar.\n")

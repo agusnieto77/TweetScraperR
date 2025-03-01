70701de33f20de1703c8909eb94f7f6014625c32
agusnieto77 <agustin.nieto77@gmail.com>
2025-03-01 01:07:02 +0100

    nuevas funciones

--- R/getUrlsTweetsCites.R
+++ R/getUrlsTweetsCites.R
@@ -1,39 +1,39 @@
-#' Get Tweets URLs Replies
+#' Get Tweets URLs Cites
 #'
 #' @description
 #' 
 #' <a href="https://lifecycle.r-lib.org/articles/stages.html#experimental" target="_blank"><img src="https://lifecycle.r-lib.org/articles/figures/lifecycle-experimental.svg" alt="[Experimental]"></a>
 #' 
-#' Esta función recupera las URLs de las respuestas a un tweet específico en Twitter (ahora X).
+#' Esta función recupera las URLs de las citas a un tweet específico en Twitter (ahora X).
 #' Utiliza web scraping para acceder a la página del tweet, iniciar sesión con las credenciales proporcionadas,
-#' y recolectar las URLs de las respuestas al tweet.
+#' y recolectar las URLs de las citas al tweet.
 #' 
 #' El proceso incluye:
 #' 1. Iniciar sesión en Twitter usando las credenciales proporcionadas.
 #' 2. Navegar a la URL del tweet especificado.
-#' 3. Extraer las URLs de las respuestas mediante scraping.
+#' 3. Extraer las URLs de las citas mediante scraping.
 #' 4. Continuar scrolling y recolectando URLs hasta alcanzar el número deseado o no encontrar nuevas URLs.
 #' 
 #' La función guarda las URLs recolectadas en un archivo RDS en el directorio especificado si el parámetro 'save' es TRUE,
 #' y las devuelve como un vector de cadenas.
 #'
-#' @param url URL del tweet del cual se quieren obtener las respuestas. Por defecto es "https://x.com/Picanumeros/status/1610715405705789442".
-#' @param n_urls El número máximo de URLs de respuestas a recuperar. Por defecto es 100.
+#' @param url URL del tweet del cual se quieren obtener las citas. Por defecto es "https://x.com/Picanumeros/status/1610715405705789442".
+#' @param n_urls El número máximo de URLs de citas a recuperar. Por defecto es 100.
 #' @param xuser Nombre de usuario de Twitter para autenticación. Por defecto es el valor de la variable de entorno del sistema USER.
 #' @param xpass Contraseña de Twitter para autenticación. Por defecto es el valor de la variable de entorno del sistema PASS.
 #' @param view Ver el navegador. Por defecto es FALSE.
 #' @param dir Directorio donde se guardará el archivo RDS con las URLs recolectadas. Por defecto es el directorio de trabajo actual.
 #' @param save Lógico. Indica si se debe guardar el resultado en un archivo RDS (por defecto TRUE).
 #'
-#' @return Un vector que contiene las URLs de las respuestas al tweet especificado.
+#' @return Un vector que contiene las URLs de las citas al tweet especificado.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' getUrlsTweetsReplies(url = "https://x.com/Picanumeros/status/1610715405705789442", n_urls = 130)
+#' getUrlsTweetsCites(url = "https://x.com/Picanumeros/status/1610715405705789442", n_urls = 130)
 #' 
 #' # Sin guardar los resultados
-#' getUrlsTweetsReplies(url = "https://x.com/Picanumeros/status/1610715405705789442", n_urls = 130, save = FALSE)
+#' getUrlsTweetsCites(url = "https://x.com/Picanumeros/status/1610715405705789442", n_urls = 130, save = FALSE)
 #' }
 #'
 #' @references
@@ -45,7 +45,7 @@
 #' @note
 #' Esta función utiliza web scraping y puede ser sensible a cambios en la estructura de la página de Twitter.
 
-getUrlsTweetsReplies <- function(
+getUrlsTweetsCites <- function(
     url = "https://x.com/Picanumeros/status/1610715405705789442",
     n_urls = 100,
     xuser = Sys.getenv("USER"),
@@ -59,7 +59,8 @@ getUrlsTweetsReplies <- function(
   success <- FALSE
   while (retry_count < max_retries && !success) {
     tryCatch({
-      urlok <- rvest::read_html_live(url)
+      urlq <- paste0(url,"/quotes")
+      urlok <- rvest::read_html_live(urlq)
       if (view) {
         urlok$view()
       }
@@ -105,7 +106,7 @@ getUrlsTweetsReplies <- function(
   if (length(tweets_urls) > 0) {
     tweets_urls <- paste0("https://x.com", tweets_urls)
     if (save) {
-      saveRDS(tweets_urls, paste0(dir, "/replies_", sub("https://x.com/(.*)/status/(.*)", "\\1_\\2", url), "_", gsub("-|:|\\.", "_", format(Sys.time(), "%Y_%m_%d_%X")), ".rds"))
+      saveRDS(tweets_urls, paste0(dir, "/cites_", sub("https://x.com/(.*)/status/(.*)", "\\1_\\2", url), "_", gsub("-|:|\\.", "_", format(Sys.time(), "%Y_%m_%d_%X")), ".rds"))
       cat("URLs procesadas y guardadas.\n")
     } else {
       cat("URLs procesadas. No se han guardado en un archivo RDS.\n")
